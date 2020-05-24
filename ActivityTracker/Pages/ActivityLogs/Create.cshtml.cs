9f1440203cd3cd1dc77db9a8e4029c20f25dd7c4
Georgi Yanakiev <georgi_87@hotmail.es>
2020-05-24 23:51:51 +0200

    Order elements in Activity dropdown in ActivityLog Create and Edit pages

--- ActivityTracker/Pages/ActivityLogs/Create.cshtml.cs
+++ ActivityTracker/Pages/ActivityLogs/Create.cshtml.cs
@@ -25,6 +25,7 @@ namespace ActivityTracker.Pages.ActivityLogs
             Activities = _context.Activity
                 .Where(a => a.ActivityValidFrom <= DateTime.Today && a.ActivityValidTo >= DateTime.Today)
                 .Include(a => a.Unit)
+                .OrderBy(a => a.ActivityName)
                 .ToList();
             ViewData["ActivityId"] = new SelectList(Activities, "ActivityId", "ActivityName");
             ViewData["strActivities"] = string.Join("|", Activities.Select(x => x.ActivityId + "#" + x.PointsPerUnit + "#" + x.MaxPointsPerDay + "#" + x.Unit.UnitName));
@@ -46,6 +47,8 @@ namespace ActivityTracker.Pages.ActivityLogs
             {
                 Activities = _context.Activity
                     .Where(a => a.ActivityValidFrom <= DateTime.Today && a.ActivityValidTo >= DateTime.Today)
+                    .Include(a => a.Unit)
+                    .OrderBy(a => a.ActivityName)
                     .ToList();
                 ViewData["ActivityId"] = new SelectList(Activities, "ActivityId", "ActivityName");
 
